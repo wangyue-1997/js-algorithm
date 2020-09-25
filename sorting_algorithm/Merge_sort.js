c497e7958e3cd80042050b26b1f22fc34b84bb6b
wangyue-1997 <wangshanjian1997@163.com>
2020-09-25 16:18:59 +0800

    sorting_algorithm🍕

--- sorting_algorithm/Merge_sort.js
+++ sorting_algorithm/Merge_sort.js
@@ -5,6 +5,17 @@
 
 const data = [1,7,8,43,2,21,7,88,9,0,-4,19,3,6];
 
+function mergeSort(arr) {
+  if(arr.length <2 ) {
+    return arr;
+  }
+  let mid = Math.floor(arr.length/2)
+  let left = arr.slice(0,mid);
+  let right = arr.slice(mid);
+
+  return merge(mergeSort(left),mergeSort(right))
+}
+
 function merge(left,right) {
   let result = [];
   while(left.length>0 && right.length>0) {
@@ -18,17 +29,6 @@ function merge(left,right) {
   if(right.length) result.push(right.shift());
 
   return result;
-}
-
-function mergeSort(arr) {
-  if(arr.length <2 ) {
-    return arr;
-  }
-  let mid = Math.floor(arr.length/2)
-  let left = arr.slice(0,mid);
-  let right = arr.slice(mid);
-
-  return merge(mergeSort(left),mergeSort(right))
 }
 
 console.log(mergeSort(data))
